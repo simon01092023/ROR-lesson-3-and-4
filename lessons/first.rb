465fe0676d6d24c2a79ad14ae003080f82c327ea
simon01092024 <simonkudus510@gmail.com>
2024-07-01 17:47:04 -0700

    lesson 3 commit

---
+++ lessons/first.rb
@@ -0,0 +1,38 @@
+first_variable = 7
+puts "Enter your name." # Here we put a prompt to the console
+name = gets.chomp    
+=begin  Here we collect the answer
+name = gets().chomp() is the verbose version
+=end
+puts "Well hello, #{name}!"  # We respond with a greeting
+:name
+:year
+:phone
+:age
+
+washington_hash = { name: "George Washington",
+  year: 1776,
+  phone: "not yet invented",
+  age: 46
+}
+
+new_hash = {}
+washington_hash[:rank] = "general"
+
+irb
+example_string = "this is a string."
+example_string.class
+:name.class
+this_hash = {}
+this_hash.class
+this_hash.methods
+quit
+
+irb
+puts %w[aaa bbb ccc ddd].inspect # an array of strings
+puts %i[aaa bbb ccc ddd].inspect # an array of symbols
+puts %q["What's that again?"] # A string that has both single and double quotes in it
+
+# Compare that with this string, that has a single quote in it but does not contain double quotes
+puts "What's that again?"
+quit
